bbc90289f156b6df7d7442d365ed803e3632d508
Alon Zakai <azakai@google.com>
2019-03-20 15:28:23 -0700

    better

--- src/passes/pass.cpp
+++ src/passes/pass.cpp
@@ -183,7 +183,9 @@ void PassRunner::addDefaultFunctionOptimizationPasses() {
   if (options.optimizeLevel >= 2 || options.shrinkLevel >= 2) {
     add("code-pushing");
   }
-
+  if (options.optimizeLevel >= 3 || options.shrinkLevel >= 1) {
+    add("copy-propagation");
+  }
   add("simplify-locals-nostructure"); // don't create if/block return values yet, as coalesce can remove copies that that could inhibit
   add("vacuum"); // previous pass creates garbage
   add("reorder-locals");
